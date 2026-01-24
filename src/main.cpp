0779520d83e37ac33ed735a1aa42e5d1b81e93fe
Gabriel Fonseca <gabriel.f.alexandre@hotmail.com>
2026-01-24 00:41:30 -0300

    can do a triangle now

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
+#include <cmath>
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+using std::endl;
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+const char *vertexShaderSource =
+    "#version 460 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}\0";
 
 int main(void) {
 
@@ -42,6 +50,42 @@ int main(void) {
     glfwPollEvents();
   }
 
+  // Trying to do a triangle
+
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+
+  unsigned int VBO; // Vertex Buffer Object
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO); // Binding the buffer
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  // Check if the shader compilation as successful
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << endl;
+  }
+  // SPLIT THE PROGRAMMM
+
+
+
+
+
+
+
+
+
+
+
+  
   glfwTerminate();
   return 0;
 }
